a73f35c51eeee87e534c0a0e00d406de818740f6
Ben Rohus <ben.rohus+student@gmail.com>
2023-04-25 14:13:48 -0500

    commit the day's lecture final

---
+++ ReviewDay - Notes API - Final/server/src/main/java/com/techelevator/notes/dao/NoteDao.java
@@ -0,0 +1,18 @@
+package com.techelevator.notes.dao;
+
+import com.techelevator.notes.model.Note;
+
+public interface NoteDao {
+
+    // create
+    public Note createNote(Note note);
+
+    // read
+    public Note getNote(int id);
+
+    // update
+    public Note updateNote(Note note);
+
+    // delete
+    public void deleteNote(int id);
+}
